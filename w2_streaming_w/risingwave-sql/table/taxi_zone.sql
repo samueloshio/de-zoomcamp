db833b1074495cf2cce659ebdd8a50571af49782
Samuel Oshio <103275722+samueloshio@users.noreply.github.com>
2024-03-12 15:05:14 +0000

    remove old w2

--- w2_streaming_w/risingwave-sql/table/taxi_zone.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE taxi_zone (
-                           location_id int,
-                           borough text,
-                           zone text,
-                           service_zone text
-);
